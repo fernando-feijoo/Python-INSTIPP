330d05592ac6315625ab2494a2430479ad5fb12c
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-07-17 17:21:12 -0500

    Concluido con exito Marca

--- UsoPlantilla/Quinto/inv/forms.py
+++ UsoPlantilla/Quinto/inv/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Categoria, SubCategoria
+from .models import Categoria, SubCategoria, Marca
 
 
 class CategoriaForm(forms.ModelForm):
@@ -38,3 +38,19 @@ class SubCategoriaForm(forms.ModelForm):
                 'class': 'form-control'
             })
         self.fields['categoria'].empty_label = "Seleccione Categoría"
+
+
+class MarcaForm(forms.ModelForm):
+    class Meta:
+        model = Marca
+        fields = ['descripcion', 'estado']
+        labels = {'descripcion': "Descripción de la MarcaS",
+                  "estado": "Estado"}
+        widget = {'descripcion': forms.TextInput}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
--- UsoPlantilla/Quinto/inv/views.py
+++ UsoPlantilla/Quinto/inv/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Categoria, SubCategoria
-from .forms import CategoriaForm, SubCategoriaForm
+from .models import Categoria, SubCategoria, Marca
+from .forms import CategoriaForm, SubCategoriaForm, MarcaForm
 # Create your views here.
 
 
@@ -27,7 +27,8 @@ class CategoriaNew(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.uc = self.request.user
         return super().form_valid(form)
-    
+
+
 class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     model = Categoria
     template_name = "inv/categoria_form.html"
@@ -39,18 +40,21 @@ class CategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         return super().form_valid(form)
-    
+
+
 class CategoriaDel(LoginRequiredMixin, generic.DeleteView):
-    model= Categoria
-    template_name= 'inv/catalogos_del.html'
-    context_object_name='obj'
-    success_url=reverse_lazy("inv:categoria_list")
+    model = Categoria
+    template_name = 'inv/catalogos_del.html'
+    context_object_name = 'obj'
+    success_url = reverse_lazy("inv:categoria_list")
+
 
 class SubCategoriaView(LoginRequiredMixin, generic.ListView):
     model = SubCategoria
     template_name = "inv/subcategoria_list.html"
     context_object_name = "obj"
     login_url = 'bases:login'
+
 
 class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):
     model = SubCategoria
@@ -63,7 +67,8 @@ class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.uc = self.request.user
         return super().form_valid(form)
-    
+
+
 class SubCategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     model = SubCategoria
     template_name = "inv/subcategoria_form.html"
@@ -75,9 +80,62 @@ class SubCategoriaEdit(LoginRequiredMixin, generic.UpdateView):
     def form_valid(self, form):
         form.instance.um = self.request.user.id
         return super().form_valid(form)
-    
+
+
 class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
-    model= SubCategoria
-    template_name= 'inv/catalogos_del.html'
-    context_object_name='obj'
-    success_url=reverse_lazy("inv:subcategoria_list")
+    model = SubCategoria
+    template_name = 'inv/catalogos_del.html'
+    context_object_name = 'obj'
+    success_url = reverse_lazy("inv:subcategoria_list")
+
+
+class MarcaView(LoginRequiredMixin, generic.ListView):
+    model = Marca
+    template_name = "inv/marca_list.html"
+    context_object_name = "obj"
+    login_url = 'bases:login'
+
+
+class MarcaNew(LoginRequiredMixin, generic.CreateView):
+    model = Marca
+    template_name = "inv/marca_form.html"
+    context_object_name = "obj"
+    form_class = MarcaForm
+    success_url = reverse_lazy("inv:marca_list")
+    login_url = "base:login"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+
+class MarcaEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Marca
+    template_name = "inv/marca_form.html"
+    context_object_name = "obj"
+    form_class = MarcaForm
+    success_url = reverse_lazy("inv:marca_list")
+    login_url = "base:login"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+
+def marca_inactivar(request, id):
+    marca = Marca.objects.filter(pk=id).first()
+    contexto = {}
+    template_name = "inv/catalogos_del.html"
+
+    if not marca:
+        return redirect("inv:marca_list")
+
+    if request.method == 'GET':
+        contexto = {'obj': marca}
+
+    if request.method == 'POST':
+        marca.estado = False
+        marca.save()
+        return redirect("inv:marca_list")
+
+    return render(request, template_name, contexto)
