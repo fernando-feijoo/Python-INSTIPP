824818648718bf9918af333eae2c506f560d364e
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-07-21 00:24:09 -0500

    Proyecto actualizado, 39 pendiente y esta todo funcional.

--- UsoPlantilla/Quinto/inv/forms.py
+++ UsoPlantilla/Quinto/inv/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Categoria, SubCategoria, Marca, UnidadMedida
+from .models import Categoria, SubCategoria, Marca, UnidadMedida, Producto
 
 
 class CategoriaForm(forms.ModelForm):
@@ -24,6 +24,7 @@ class SubCategoriaForm(forms.ModelForm):
         queryset=Categoria.objects.filter(estado=True)
         .order_by('descripcion')
     )
+
     class Meta:
         model = SubCategoria
         fields = ['categoria', 'descripcion', 'estado']
@@ -55,6 +56,7 @@ class MarcaForm(forms.ModelForm):
                 'class': 'form-control'
             })
 
+
 class UMForm(forms.ModelForm):
     class Meta:
         model = UnidadMedida
@@ -69,3 +71,21 @@ class UMForm(forms.ModelForm):
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
+
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['codigo', 'codigo_barra', 'descripcion', 'estado', 'precio',
+                  'existencia', 'ultima_compra', 'marca', 'subcategoria', 'unidad_medida']
+        exclude = ['um', 'fm', 'uc', 'fc']
+        widget = {'descripcion': forms.TextInput()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['ultima_compra'].widget.attrs['readonly'] = True
+        self.fields['existencia'].widget.attrs['readonly'] = True
