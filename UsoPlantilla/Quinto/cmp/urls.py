f1a064dbfe7ea7a336e9a0258be7df5f0c49535e
Fernando PC-Escritorio <fernando95f4@gmail.com>
2023-07-21 17:02:46 -0500

    Actualizacion, queda algo pendiente con la ruta de proveedor

---
+++ UsoPlantilla/Quinto/cmp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from .views import ProveedorView, ProveedorNew, ProveedorEdit
+
+urlpatterns = [
+
+    path('proveedores/', ProveedorView.as_view(), name='proveedor_list'),
+    path('proveedores/new', ProveedorNew.as_view(), name='proveedor_new'),
+    path('proveedores/edit/<int:pk>', ProveedorEdit.as_view(), name='proveedor_edit'),
+    # path('proveedores/inactivar/<int:id>', proveedor_inactivar, name='proveedor_inactivar'),
+
+]
